ea32f19ace10a0b3115c67faf40347f2bb00554a
SmArT03 <smart5449@gmail.com>
2016-05-26 11:04:25 +0300

    changed auth

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,16 +1,15 @@
-<?php
+<?php 
 
-
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
-
-class DefaultController extends Controller
-{
-    /**
-     * @Route("/admin")
-     */
-    public function adminAction()
-    {
-        return new Response('<html><body>Admin page!</body></html>');
-    }
-}
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+//use Symfony\Component\HttpFoundation\Response;
+//
+//class DefaultController extends Controller
+//{
+//    /**
+//     * @Route("/admin")
+//     */
+//    public function adminAction()
+//    {
+//        return new Response('<html><body>Admin page!</body></html>');
+//    }
+//}
